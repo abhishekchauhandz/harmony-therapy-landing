72d41cbcd73f2b26daf23980e0e387f91611f2e6
abhishekshanka28 <abhishekshankar28@gmail.com>
2024-09-30 19:10:29 +0530

    good changes

--- src/app/components/Features.jsx
+++ src/app/components/Features.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Features() {
     return (
         <>
-            <div>
-                <h1 className="font-bold text-3xl md:text-5xl text-center">Costs <span className="text-[#33C9D2]">0</span> at <span className="text-[#33C9D2]">Harmony Therapy</span></h1>
-                <div className="flex flex-col md:flex-row justify-center md:justify-around items-center py-10 space-y-10 md:space-y-0">
-                    <div className="relative flex-shrink-0 pt-20">
+            <div className="md:p-0 p-4">
+                <h1 className="font-bold text-4xl md:text-5xl text-center">Costs <span className="text-[#33C9D2]">0</span> at <span className="text-[#33C9D2]">Harmony Therapy</span></h1>
+                <div className="flex flex-col md:flex-row justify-center md:justify-around items-center md:py-0 mt-10 md:mt-0 space-y-10 md:space-y-0">
+                    <div className="relative flex-shrink-0 md:pt-10 pt-20 md:mt-0 mt-10">
                         <Image src='/image-bg.png' width={480} height={480} />
-                        <Image src='/modal.png' width={320} height={412} className="absolute left-20 bottom-20" />
+                        <Image src='/modal.png' width={320} height={412} className="absolute md:absolute md:left-20 left-10 bottom-20" />
                     </div>
                     <div className="w-full md:w-1/2">
                         <h1 className="font-bold text-3xl md:text-5xl font-montserrat">Mood Journal & Tracker 😊</h1>
@@ -22,14 +22,14 @@ export default function Features() {
                     </div>
                     <div className="relative flex-shrink-0 pb-10">
                         <Image src='/image-bg.png' width={480} height={480} />
-                        <Image src='/modal1.png' width={616} height={411} className="absolute right-30 bottom-20" />
+                        <Image src='/modal1.png' width={616} height={411} className="absolute md:absolute md:right-30 right-30 top-10" />
                     </div>
 
                 </div>
-                <div className="flex flex-col md:flex-row justify-center md:justify-around items-center py-10 space-y-10 md:space-y-0">
-                    <div className="relative flex-shrink-0 pt-20 mt-20">
+                <div className="flex flex-col md:flex-row justify-center md:justify-around items-center md:py-0 py-10 space-y-10 md:space-y-0">
+                    <div className="relative flex-shrink-0 md:pt-0 pt-20 md:mt-0 mt-20">
                         <Image src='/image-bg.png' width={480} height={480} />
-                        <Image src='/modal2.png' width={397} height={514} className="absolute left-30 bottom-20" />
+                        <Image src='/modal2.png' width={397} height={514} className="absolute md:absolute md:right-20 right-10 bottom-20" />
                     </div>
                     <div className="w-full md:w-1/2">
                         <h1 className="font-bold text-3xl md:text-5xl font-montserrat">Access to All <span className="text-[#33C9D2]">Personalized Resources</span></h1>
@@ -43,7 +43,7 @@ export default function Features() {
                     </div>
                     <div className="relative flex-shrink-0 pb-10">
                         <Image src='/image-bg.png' width={480} height={480} />
-                        <Image src='/modal3.png' width={514} height={412} className="absolute right-25 top-0" />
+                        <Image src='/modal3.png' width={514} height={412} className="absolute md:absolute md:right-25 left-0 top-0" />
                     </div>
 
                 </div>
