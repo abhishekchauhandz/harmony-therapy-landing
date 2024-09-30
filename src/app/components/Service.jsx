63dde7d2612c4d8d03d8badfcd01431308c675bd
abhishekshanka28 <abhishekshankar28@gmail.com>
2024-09-30 23:00:37 +0530

    good changes

--- src/app/components/Service.jsx
+++ src/app/components/Service.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import TestimonialCard from "@/components/Testimonials";
+import TestimonialCard from '../../../src/components/Testimonials'
 
 
 export default function Service() {
