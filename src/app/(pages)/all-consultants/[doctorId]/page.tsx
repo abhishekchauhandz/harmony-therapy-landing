05fc32632912f7a02e094443e222f60e711c33b2
Abhishek chauhan <abhishekchauhandz@gmail.com>
2024-10-07 16:08:51 +0530

    dcotor id

--- src/app/(pages)/all-consultants/[doctorId]/page.tsx
+++ src/app/(pages)/all-consultants/[doctorId]/page.tsx
@@ -50,76 +50,75 @@ const StaffDetail: React.FC = () => {
     }
 
     return (
-        <div className="flex justify-center items-center py-5 w-full pt-36 md:px-10 px-4 relative">
-            <div className="flex flex-col md:flex-row items-center justify-center gap-10 md:py-0 py-5 max-w-6xl w-full">
-                {/* Doctor Card */}
-                <div className="rounded-3xl p-3 m-2 w-full md:w-[320px] h-[350px] shadow-md hover:shadow-lg transition-shadow cursor-pointer flex flex-col justify-between bg-white">
-                    <div className="relative w-full h-[80%] rounded-3xl">
-                        <Image
-                            src="/altImage.png"
-                            alt="Doctor"
-                            fill
-                            objectFit="cover"
-                            className="rounded-3xl"
-                        />
+        <div className="flex justify-center items-center py-12 mt-10 pt-36 md:px-10 px-4 relative">
+            <div className="md:w-[60%] w-full p-3 py-4 rounded-3xl flex flex-col md:gap-5 md:flex-row shadow-md hover:shadow-lg transition-shadow cursor-pointer flex bg-white">
+                    <div className='flex flex-col  md:w-[320px] h-[350px]'>
+                        <div className="relative w-full h-[80%] rounded-3xl">
+                            <Image
+                                src="/altImage.png"
+                                alt="Doctor"
+                                fill
+                                objectFit="cover"
+                                className="rounded-3xl"
+                            />
+                        </div>
+                        <div className="text-center mt-3">
+                            <h3 className="text-sm md:text-xl font-bold text-gray-800">
+                                {doctorData.doctorName || doctorData.username}
+                            </h3>
+                            <p className="text-sm text-gray-600">
+                                {doctorData.role}
+                            </p>
+                        </div>
                     </div>
-                    <div className="text-center mt-3">
-                        <h3 className="text-sm md:text-xl font-bold text-gray-800">
-                            {doctorData.doctorName || doctorData.username}
-                        </h3>
-                        <p className="text-sm text-gray-600">
-                            {doctorData.role}
-                        </p>
-                    </div>
-                </div>
+                    <div className="px-4 md:w-1/2 w-full flex flex-col gap-5 md:mt-5">
+                        <h2 className="font-bold text-2xl text-[#33C9D2] text-center">Details</h2>
+                        <div className="grid grid-cols-2 gap-y-4 md:gap-x-8 gap-x-0">
 
-                {/* Doctor Details */}
-                <div className="md:px-10 px-4 md:w-[400px] w-full flex flex-col justify-between">
-                    <h6 className="font-bold text-2xl text-[#33C9D2] mb-10 text-center">Details</h6>
-                    <div className="grid grid-cols-2 gap-y-4 md:gap-x-8 gap-x-0">
+                            {/* Gender */}
+                            <div className="col-span-1">
+                                <p className="text-gray-700 font-medium">Gender:</p>
+                            </div>
+                            <div className="col-span-1">
+                                <p>{doctorData.gender}</p>
+                            </div>
 
-                        {/* Gender */}
-                        <div className="col-span-1">
-                            <p className="text-gray-700 font-medium">Gender:</p>
-                        </div>
-                        <div className="col-span-1">
-                            <p>{doctorData.gender}</p>
-                        </div>
+                            {/* Status */}
+                            <div className="col-span-1">
+                                <p className="text-gray-700 font-medium">Status:</p>
+                            </div>
+                            <div className="col-span-1">
+                                <p>{doctorData.status}</p>
+                            </div>
 
-                        {/* Status */}
-                        <div className="col-span-1">
-                            <p className="text-gray-700 font-medium">Status:</p>
-                        </div>
-                        <div className="col-span-1">
-                            <p>{doctorData.status}</p>
-                        </div>
+                            {/* Max Education */}
+                            <div className="col-span-1">
+                                <p className="text-gray-700 font-medium">Max. Education:</p>
+                            </div>
+                            <div className="col-span-1">
+                                <p>{doctorData.maximumEducation}</p>
+                            </div>
 
-                        {/* Max Education */}
-                        <div className="col-span-1">
-                            <p className="text-gray-700 font-medium">Max. Education:</p>
-                        </div>
-                        <div className="col-span-1">
-                            <p>{doctorData.maximumEducation}</p>
-                        </div>
+                            {/* Price per session */}
+                            <div className="col-span-1">
+                                <p className="text-gray-700 font-medium">Price per session:</p>
+                            </div>
+                            <div className="col-span-1">
+                                <p>{doctorData.pricePerSession}</p>
+                            </div>
 
-                        {/* Price per session */}
-                        <div className="col-span-1">
-                            <p className="text-gray-700 font-medium">Price per session:</p>
-                        </div>
-                        <div className="col-span-1">
-                            <p>{doctorData.pricePerSession}</p>
-                        </div>
-
-                        {/* Experience */}
-                        <div className="col-span-1">
-                            <p className="text-gray-700 font-medium">Experience:</p>
-                        </div>
-                        <div className="col-span-1">
-                            <p>{doctorData.experience}</p>
+                            {/* Experience */}
+                            <div className="col-span-1">
+                                <p className="text-gray-700 font-medium">Experience:</p>
+                            </div>
+                            <div className="col-span-1">
+                                <p>{doctorData.experience}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
+            <div className=" items-center justify-center gap-10 md:py-0 py-5">
+                
             </div>
         </div>
     );
