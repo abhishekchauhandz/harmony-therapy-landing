97b95f56d7841a006c5b837a179b0590492ddc1a
Abhishek chauhan <abhishekchauhandz@gmail.com>
2024-10-31 00:17:56 +0530

    done

--- src/app/components/CostSection.jsx
+++ src/app/components/CostSection.jsx
@@ -6,12 +6,12 @@ export default function CostSection() {
         <div className="flex justify-center items-center py-24 px-0 md:px-0 md:mt-16">
             <div className="cost w-full md:w-[80%] h-[30vh] md:h-[70vh] md:rounded-3xl md:p-6 md:px-8 relative bg-[url('/mobile.png')] md:bg-contain bg-cover bg-center bg-no-repeat flex items-center justify-end" data-aos="fade-right">
                 <div className="cost-div md:w-[60%] w-full h-full flex flex-col justify-center md:bg-transparent p-5 rounded-2xl">
-                    <div className="head-ings">
+                    <div className="head-ings md:mr-4">
                         <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl text-white text-right">Costing you nothing</h1>
                         <h2 className="font-bold text-xl sm:text-2xl md:text-3xl text-[#FFA05D] text-right">bringing pure harmony.</h2>
                     </div>
 
-                    <p className="font-medium text-base sm:text-lg md:text-xl mt-4 md:mb-16 text-white text-right pl-5 sm:pl-10 md:pl-16">Install the app for free and unlock premium resources at no cost. Connect with your therapist right here.</p>
+                    <p className="font-medium text-base sm:text-lg md:text-xl mt-4 md:mb-16 text-white text-right pl-5 sm:pl-10 md:pl-16 md:mr-4">Install the app for free and unlock premium resources at no cost. Connect with your therapist right here.</p>
                     
                     {/* Uncomment to add the links back */}
                     {/* <div className="flex gap-5 md:pb-5 md:mt-0 mt-10">
