9e2ee60755defcc915e9fc15ec835ca4d708f79b
Abhishek chauhan <abhishekchauhandz@gmail.com>
2024-10-30 22:13:48 +0530

    final chnages

--- src/app/components/CostSection.jsx
+++ src/app/components/CostSection.jsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export default function CostSection() {
     return (
         <>
-            <div className="flex justify-center items-center py-24 px-4 md:px-0 md:mt-16">
-                <div className="w-full md:w-[80%] h-auto md:h-[70vh] rounded-3xl p-6 md:px-8 relative md:bg-[url('/mobile.png')] bg-cover bg-center bg-no-repeat flex justify-end" data-aos="fade-right">
-                    <div className="md:w-[60%] w-full h-full flex flex-col justify-center md:bg-transparent bg-white p-5 rounded-2xl">
+            <div className="flex justify-center items-center py-24 px-0 md:px-0 md:mt-16">
+                <div className="w-full md:w-[80%] h-[30vh] md:h-[70vh] md:rounded-3xl md:p-6 md:px-8 relative bg-[url('/mobile.png')] md:bg-cover bg-cover bg-center bg-no-repeat flex items-center justify-end" data-aos="fade-right">
+                    <div className="md:w-[60%] w-full h-full flex flex-col justify-center md:bg-transparent p-5 rounded-2xl">
                         <div>
-                            <h1 className="font-bold text-2xl md:text-5xl md:text-white md:text-right">Costing you nothing</h1>
-                            <h2 className="font-bold text-xl md:text-4xl text-[#FFA05D] md:text-right">bringing pure harmony.</h2>
+                            <h1 className="font-bold text-2xl md:text-5xl text-white text-right">Costing you nothing</h1>
+                            <h2 className="font-bold text-xl md:text-4xl text-[#FFA05D] text-right">bringing pure harmony.</h2>
                         </div>
 
-                        <p className="font-medium text-base md:text-xl mt-4 md:mb-16 md:text-white md:text-right md:pl-6">Install the app for free and unlock premium resources at no cost. Connect with your therapist right here.
+                        <p className="font-medium text-base md:text-xl mt-4 md:mb-16 text-white md:text-right pl-40 md:pl-6 text-right">Install the app for free and unlock premium resources at no cost. Connect with your therapist right here.
                         </p>
                         {/* <div className="flex gap-5 md:pb-5 md:mt-0 mt-10">
                             <Link href="https://play.google.com/">
